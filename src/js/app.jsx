61a70110872a1fd5e14de64d682d2cd099efb4e6
Godfrey Martinez <godmartinz@gmail.com>
2019-06-21 16:19:33 -0700

    adds initial project files

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { SearchHistory } from './searchHistory';
+import { CityInfo } from './cityInfo';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
+        <div className="jumbotron"><h1>SDCS Weather Application</h1><h5>Always know if you'll need an umbrella!</h5>
+        </div>
+        <div>
+        <div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Choose a City" aria-label="city-choice"/>
+  <div className="input-group-append">
+    <button className="btn btn-outline-secondary" type="button">Button</button>
+  </div>
+</div>
+        </div>
+        <CityInfo/>
+        <SearchHistory/>
       </div>
     );
   }
